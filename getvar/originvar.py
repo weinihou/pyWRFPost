a0ea7ae689372406851a92a63ed0429c9705adcb
Shiwei Sun <sunshiwei.atmo@gmail.com>
2016-12-11 22:28:56 +0800

    Add some files
    Add some files and design the structure.

---
+++ getvar/originvar.py
@@ -0,0 +1,12 @@
+#==============================================================================
+# Load the original variables from wrfout files.
+#==============================================================================
+'''
+This is the first step of WRF post processing, even the variables' loading.
+
+All the variables here are just in the output files directly, so we just open 
+the files and get the vars.
+
+'''
+a
+    
